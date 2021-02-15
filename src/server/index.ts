00e91b0af5c033cc54b47f3d6461e9c4a3ae18f8
Robin Karlsson <robin98.karlsson@gmail.com>
2021-02-15 21:20:41 +0200

    Move JWT_SIGNING_SECRET to .env

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import 'reflect-metadata';
 import { useContainer, ConnectionOptions, createConnection } from 'typeorm';
 import { Container } from 'typedi';
--- src/server/services/auth-service.ts
+++ src/server/services/auth-service.ts
@@ -1,6 +1,6 @@
 import * as jwt from 'jsonwebtoken';
 
-const JWT_SIGNING_SECRET = 'this_secret_should_come_from_hidden_env_files_and_never_committed_to_repo';
+const JWT_SIGNING_SECRET = process.env.JWT_SIGNING_SECRET;
 
 const generateJwtForUserId = (userId: string) => jwt.sign({ userId }, JWT_SIGNING_SECRET);
 
